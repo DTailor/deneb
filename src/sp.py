51da913784d322ceaee04b10c43bc60f6d6b94aa
DTailor <dann.croitoru@gmail.com>
2018-03-04 21:13:49 +0200

    add caching by token for spotify clients

--- src/sp.py
+++ src/sp.py
@@ -1,6 +1,7 @@
 """Spotify connection handling"""
 
 import os
+import weakref
 
 import spotipy
 from spotipy import util
@@ -10,6 +11,8 @@ from spotipy.client import SpotifyException
 assert os.environ['SPOTIPY_CLIENT_ID']
 assert os.environ['SPOTIPY_CLIENT_SECRET']
 assert os.environ['SPOTIPY_REDIRECT_URI']
+
+_SPOTI_CACHE = weakref.WeakValueDictionary()
 
 
 def fetch_token(username):
@@ -28,11 +31,18 @@ def get_sp_client(username, token=None):
     """returns new sp client plus the token for it"""
     if token is None:
         token = fetch_token(username)
-    sp_client = spotipy.Spotify(auth=token)
+
+    if token in _SPOTI_CACHE:
+        sp_client = _SPOTI_CACHE[token]
+    else:
+        sp_client = spotipy.Spotify(auth=token)
+
     try:
+        # check client stil good
         sp_client.current_user()
     except SpotifyException:
         # token may expire, ask for new
         token = fetch_token(username)
         sp_client = spotipy.Spotify(auth=token)
+        _SPOTI_CACHE[token] = sp_client
     return sp_client, token
