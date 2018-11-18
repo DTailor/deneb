28b37abdcbe91bedb231fb6ab01da6152326e40d
DTailor <dann.croitoru@gmail.com>
2018-11-18 21:36:20 +0200

    group first albums then tracks in playlist

--- src/weekler.py
+++ src/weekler.py
@@ -1,13 +1,14 @@
 """Create spotify playlist with weekly new releases"""
 import calendar
 from datetime import datetime as dt
+from itertools import chain
 from math import ceil
-from typing import Any, List, Optional, Tuple
+from typing import List, Optional, Tuple
 
 from db import Album, get_or_create_user
 from logger import get_logger
 from sp import Spotter, get_sp_client
-from tools import fetch_all, is_present
+from tools import DefaultOrderedDict, clean, fetch_all, grouper, is_present
 
 _LOGGER = get_logger(__name__)
 
@@ -59,16 +60,26 @@ def generate_tracks_to_add(
     sp: Spotter,
     db_tracks: List[Album],
     pl_tracks: List[dict]
-) -> List[Tuple[str, Any]]:
+) -> Tuple[List, List]:
     """return list of tracks to be added"""
     already_present_tracks = {a["track"]["name"] for a in pl_tracks}
 
-    tracks = []     # type: List[Tuple[str, Any]]
+    tracks = {
+        "album": DefaultOrderedDict(list),
+        "track": DefaultOrderedDict(list)
+    }
     for item in db_tracks:
         check_tracks = [item]
         if item.type == "album":
             check_tracks = get_album_tracks(sp, item)
+        else:
+            try:
+                check_tracks = [sp.client.track(item.uri)]
+            except Exception as exc:
+                _LOGGER.exception(f"failed fetch track item {item} with: {exc}")
+                continue
         for track in check_tracks:
+            track_type = item.type
             if isinstance(track, Album):
                 track_name = track.name
                 track_id = track.spotify_id
@@ -77,8 +88,13 @@ def generate_tracks_to_add(
                 track_id = track["id"]
             if track_name in already_present_tracks:
                 continue
-            tracks.append((track_id, track))
-    return tracks
+            else:
+                already_present_tracks.add(track_name)
+            if len(check_tracks) <= 3:
+                # put all <= 3 track albums to tracks list
+                track_type = "track"
+            tracks[track_type][track_id].append(track)
+    return tracks["album"], tracks["track"]
 
 
 def main(username, fb_id):
@@ -102,13 +118,20 @@ def main(username, fb_id):
     if not playlist:
         playlist = sp.client.user_playlist_create(sp.username, playlist_name)
     playlist_tracks = get_tracks(sp, playlist)
-    tracks = generate_tracks_to_add(sp, week_tracks_db, playlist_tracks)
-    for track_id, track in tracks:
-        # at the moment add every single track separately
-        # there are some wrong ids on the way which break the
-        # whole batch of ids
-        # TODO: validate somehow the ids from the batch
+    albums, tracks = generate_tracks_to_add(sp, week_tracks_db, playlist_tracks)
+    # for track_id, track in tracks:
+    #     # at the moment add every single track separately
+    #     # there are some wrong ids on the way which break the
+    #     # whole batch of ids
+    #     # TODO: validate somehow the ids from the batch
+    #     try:
+
+    for album_ids in grouper(100, chain(albums.keys(), tracks.keys())):
+        album_ids = clean(album_ids)
         try:
-            sp.client.user_playlist_add_tracks(sp.username, playlist['uri'], [track_id])
+            sp.client.user_playlist_add_tracks(sp.username, playlist['uri'], album_ids)
         except Exception as exc:
-            _LOGGER.exception(f"trying to add {track} tracks: ; failed: {exc}")
+            _LOGGER.exception(f"add to playlist '{album_ids}' failed with: {exc}")
+
+
+main('dann.croitoru', 'dann.croitoru1111')
