c95daf44ac93bc25c1932f37334a19b286119031
DTailor <dann.croitoru@gmail.com>
2018-12-16 21:57:08 +0200

    add fabric

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+import os
+
+from fabric import Connection, task
+
+SSH_USER = os.environ['SSH_USER']
+SSH_HOST = os.environ['SSH_HOST']
+
+
+@task
+def deploy(c):
+    captain = Connection(f"{SSH_USER}@{SSH_HOST}")
+    with captain.cd("/apps/deneb/"):
+        captain.run("git reset --hard HEAD")
+        captain.run("git fetch -ap")
+        captain.run("git pull")
+        captain.run("pipenv sync")
+        captain.run("pipenv clean")
+
+
+@task
+def run_all(c):
+    captain = Connection(f"{SSH_USER}@{SSH_HOST}")
+    with captain.cd("/apps/deneb/"):
+        captain.run("pipenv run python -m deneb full-run")
