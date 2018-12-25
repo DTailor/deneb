23d6089c69fc8a996c5f3c3912bdf1ecee97b77f
DTailor <dann.croitoru@gmail.com>
2018-12-25 18:38:40 +0200

    split long messages

--- deneb/chatbot/message.py
+++ deneb/chatbot/message.py
@@ -2,10 +2,21 @@ import requests
 
 from deneb.logger import get_logger
 from deneb.structs import FBAltert
-from deneb.tools import grouper
 
 
 _LOGGER = get_logger(__name__)
+
+
+def grouper(size, data):
+    """helper to splita data in chunks of size"""
+    to_return = ""
+    for line in data.splitlines():
+        if len(to_return) + len(line) <= size:
+            to_return = f"{to_return}{line}\n"
+        else:
+            yield to_return
+            to_return = ""
+    yield to_return
 
 
 def send_message(
@@ -16,6 +27,7 @@ def send_message(
     """send fb user text message"""
     fb_token = {"access_token": fb_alert.key}
 
+    # max message size is 2000
     for msg_chunk in grouper(2000, data):
         clean_chunk = [a for a in msg_chunk if a is not None]
         contents = {
