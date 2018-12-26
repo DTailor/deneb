7438f895c9222a8fa4797d7b3d9a6abccceb744f
DTailor <dann.croitoru@gmail.com>
2018-12-26 17:49:27 +0200

    fix-up tests

--- tests/unit/fixtures/mocks.py
+++ tests/unit/fixtures/mocks.py
@@ -1,6 +1,8 @@
-import pytest
-import os
 import json
+import os
+
+import pytest
+from mock import MagicMock
 
 
 @pytest.fixture
@@ -9,3 +11,32 @@ def playlist() -> dict:
     with open(playlist_path, 'r') as f:
         playlist_data = f.read()
     return json.loads(playlist_data)
+
+
+def get_track() -> dict:
+    track_path = os.path.join(os.path.dirname(__file__), "examples", "track.json")
+    with open(track_path, 'r') as f:
+        track_data = f.read()
+    return json.loads(track_data)
+
+
+@pytest.fixture
+def track() -> dict:
+    return get_track()
+
+
+@pytest.fixture
+def album_db():
+    album = MagicMock()
+    album.name = 'Test Album'
+
+    artist = MagicMock()
+    artist.name = 'Test Artist'
+
+    album.artists = MagicMock()
+    album.artists.return_value = [artist]
+
+    album.human_name = MagicMock()
+    album.human_name.return_value = 'Test Artist - Test Album'
+
+    return album
--- tests/unit/test_structs.py
+++ tests/unit/test_structs.py
@@ -1,27 +1,63 @@
 # flake8: noqa
-from deneb.structs import SpotifyStats
-from tests.unit.fixtures.mocks import playlist
+from deneb.structs import SpotifyStats, AlbumTracks
+from tests.unit.fixtures.mocks import playlist, track, album_db
 
 
 class TestSpotifyStats:
+    def test_humanize_track(self, track):
+        noalbum_tracks = AlbumTracks(parent=None, tracks=[track,])
 
-    def test_one_added_track(self, playlist):
-        one_track = ["one-track-lol"]
+        assert SpotifyStats.humanize_track(noalbum_tracks) == 'Test Track - Test Track'
 
-        ss = SpotifyStats('test_user', playlist, one_track)
+    def test_describe_added_album(self, playlist, album_db, track):
+        album_tracks = AlbumTracks(parent=album_db, tracks=[track, track, track])
+        stats = SpotifyStats(
+            "test-id", playlist, {"tracks": [], "albums": [album_tracks]}
+        )
 
-        expected_string = "I added 1 track to Test Playlist"
-        assert ss.describe() == expected_string
+        expected = [
+            "Test Playlist",
+            "Albums:",
+            "- [Test Artist - Test Album]",
+            "   * Test Track",
+            "   * Test Track",
+            "   * Test Track",
+            "",
+        ]
 
-    def test_more_added_tracks(self, playlist):
-        more_tracks = ["one", "two"]
+        output = stats.describe()
 
-        ss = SpotifyStats("test_user", playlist, more_tracks)
+        for line1, line2 in zip(output.splitlines(), expected):
+            assert line1 == line2
 
-        expected_string = "I added 2 tracks to Test Playlist"
-        assert ss.describe() == expected_string
+    def test_describe_added_track(self, playlist, track):
+        album_tracks = AlbumTracks(parent=None, tracks=[track,])
+        stats = SpotifyStats(
+            "test-id", playlist, {"tracks": [album_tracks], "albums": []}
+        )
 
-    def test_empty_added_tracks(self, playlist):
+        expected = ['Test Playlist', 'Tracks:', ' * Test Track - Test Track']
+
+        output = stats.describe()
+
+        for line1, line2 in zip(output.splitlines(), expected):
+            assert line1 == line2
+
+    def test_describe_added_tracks_and_albums(self, playlist, album_db, track):
+        album_tracks = AlbumTracks(parent=album_db, tracks=[track, track, track])
+        just_tracks = AlbumTracks(parent=None, tracks=[track, track, track])
+        stats = SpotifyStats(
+            "test-id", playlist, {"tracks": [just_tracks], "albums": [album_tracks]}
+        )
+
+        expected = ['Test Playlist', 'Albums:', '- [Test Artist - Test Album]', '   * Test Track', '   * Test Track', '   * Test Track', '', 'Tracks:', ' * Test Track - Test Track']
+
+        output = stats.describe()
+
+        for line1, line2 in zip(output.splitlines(), expected):
+            assert line1 == line2
+
+    def test_describe_nothing_added(self, playlist):
         valid_responses = [
             "Uhh, sorry, no releases today for you.",
             "Didn't find anything new today",
@@ -29,6 +65,6 @@ class TestSpotifyStats:
             "No adds, you should follow more artists",
         ]
 
-        ss = SpotifyStats("test_user", playlist, [])
+        ss = SpotifyStats("test-id", playlist, {"albums": [], "tracks": []})
 
         assert ss.describe() in valid_responses
