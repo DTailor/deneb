abb086c92d022a6258bd5a3def71eaa38e03085b
DTailor <dann.croitoru@gmail.com>
2018-12-26 20:40:21 +0200

    add fetch artsists test

--- tests/unit/fixtures/mocks.py
+++ tests/unit/fixtures/mocks.py
@@ -2,20 +2,20 @@ import json
 import os
 
 import pytest
-from mock import MagicMock
+from unittest.mock import MagicMock
 
 
 @pytest.fixture
 def playlist() -> dict:
     playlist_path = os.path.join(os.path.dirname(__file__), "examples", "playlist.json")
-    with open(playlist_path, 'r') as f:
+    with open(playlist_path, "r") as f:
         playlist_data = f.read()
     return json.loads(playlist_data)
 
 
 def get_track() -> dict:
     track_path = os.path.join(os.path.dirname(__file__), "examples", "track.json")
-    with open(track_path, 'r') as f:
+    with open(track_path, "r") as f:
         track_data = f.read()
     return json.loads(track_data)
 
@@ -25,18 +25,49 @@ def track() -> dict:
     return get_track()
 
 
+def get_artist(name: str = "") -> dict:
+    artist_path = os.path.join(os.path.dirname(__file__), "examples", "artist.json")
+    with open(artist_path, "r") as f:
+        artist_data = f.read()
+    artist_data = artist_data.replace("TESTID", name.lower())
+    artist_data = artist_data.replace("TEST_ID", name.capitalize())
+    return json.loads(artist_data)
+
+
 @pytest.fixture
-def album_db():
+def artist() -> dict:
+    return get_artist()
+
+
+@pytest.fixture
+def album_db() -> MagicMock:
     album = MagicMock()
-    album.name = 'Test Album'
+    album.name = "Test Album"
 
     artist = MagicMock()
-    artist.name = 'Test Artist'
+    artist.name = "Test Artist"
 
     album.artists = MagicMock()
     album.artists.return_value = [artist]
 
     album.human_name = MagicMock()
-    album.human_name.return_value = 'Test Artist - Test Album'
+    album.human_name.return_value = "Test Artist - Test Album"
 
     return album
+
+
+@pytest.fixture
+def sp_following() -> MagicMock:
+    sp = MagicMock()
+
+    # config returned artists
+    sp.client.current_user_followed_artists = MagicMock()
+    sp.client.current_user_followed_artists.return_value = {
+        "artists": {"items": [get_artist("1")], "next": True}
+    }
+
+    sp.client.next = MagicMock()
+    sp.client.next.return_value = {
+        "artists": {"items": [get_artist("2")], "next": False}
+    }
+    return sp
