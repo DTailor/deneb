3b63eb88d2610f8b808ebb766ffcb3046c713a58
DTailor <dann.croitoru@gmail.com>
2019-02-10 18:12:55 +0200

    fix test

--- tests/unit/test_user_update.py
+++ tests/unit/test_user_update.py
@@ -10,8 +10,8 @@ from tests.unit.fixtures.mocks import sp_following
 class TestFetchArtist:
     # implying only 2 artists come at this point
 
-    def test_fetch_artist_ok(self, sp_following):
-        artists = fetch_artists(sp_following)
+    async def test_fetch_artist_ok(self, sp_following):
+        artists = await fetch_artists(sp_following)
         assert len(artists) == 2
 
 
