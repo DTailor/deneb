4ecc1c2a1e43706e70ae336e8e21d29f36b2ad8c
DTailor <dann.croitoru@gmail.com>
2019-04-15 21:30:22 +0300

    unit test for no follows

--- tests/unit/test_user_update.py
+++ tests/unit/test_user_update.py
@@ -1,16 +1,15 @@
 # flake8: noqa
-from unittest.mock import MagicMock
+from unittest import mock
 
-from deneb.user_update import (
-    extract_lost_follows_artists,
-    extract_new_follows_objects,
-    fetch_artists,
-    check_follows,
-)
-from tests.unit.fixtures.mocks import sp_following
 import pytest
 from aiomock import AIOMock
+
 from deneb.db import Artist
+from deneb.user_update import (
+    check_follows, extract_lost_follows_artists, extract_new_follows_objects,
+    fetch_artists, fetch_user_followed_artists
+)
+from tests.unit.fixtures.mocks import get_artist, sp_following
 
 
 class TestFetchArtist:
@@ -40,9 +39,9 @@ class TestExtractLostFollowsArtists:
     def test_one_lost(self):
         followed_artists = [{"id": "stay"}, {"id": "stay2"}]
         current_following = [
-            MagicMock(spotify_id="stay"),
-            MagicMock(spotify_id="stay2"),
-            MagicMock(spotify_id="leave"),
+            mock.MagicMock(spotify_id="stay"),
+            mock.MagicMock(spotify_id="stay2"),
+            mock.MagicMock(spotify_id="leave"),
         ]
         lost = extract_lost_follows_artists(followed_artists, current_following)
         assert len(lost) == 1
@@ -74,3 +73,25 @@ class TestCheckFollows:
 
         # if contains, list must be empty else the artist
         assert len([a for a in contains_follow if a == False]) == len(lost_follows)
+
+
+class TestFetchUserFollowedArtists:
+    @pytest.mark.asyncio
+    async def test_no_follows(self):
+        user = AIOMock()
+        user.artists.filter.async_return_value = []
+        sp = AIOMock()
+
+        with mock.patch(
+            "deneb.user_update.fetch_artists", new=AIOMock()
+        ) as mock_fetch_artists:
+            with mock.patch(
+                "deneb.user_update.extract_new_follows_objects"
+            ) as mock_extract_new_follows_objects:
+                mock_fetch_artists.async_return_value = []
+                mock_extract_new_follows_objects.return_value = []
+
+                new, lost = await fetch_user_followed_artists(user, sp, False)
+
+                mock_fetch_artists.assert_called_once_with(sp)
+                mock_extract_new_follows_objects.assert_called_once_with([], [])
