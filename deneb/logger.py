68db9582cca9568e3bba4f844e107577264c8e15
DTailor <dann.croitoru@gmail.com>
2019-06-30 20:22:57 +0300

    update logger

--- deneb/logger.py
+++ deneb/logger.py
@@ -1,10 +1,11 @@
 """Logger config for project"""
 import logging
 import os
-from typing import Any
+from typing import Any, Dict  # noqa
 
 import logzero
 import sentry_sdk
+from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 
 from deneb.config import VERSION
 
@@ -24,16 +25,18 @@ def get_logger(name: str) -> Any:
     )
 
     sentry_url = os.environ.get("SENTRY_URL")
-    kwargs = {}
+    sentry_kwargs = {}  # type: Dict[str, Any]
+
+    sentry_kwargs["integrations"] = [AioHttpIntegration()]
     environ = os.environ.get("ENVIRON")
     if environ:
-        kwargs["environment"] = environ
+        sentry_kwargs["environment"] = environ
 
     server_name = os.environ.get("SERVER_NAME")
     if server_name:
-        kwargs["server_name"] = server_name
+        sentry_kwargs["server_name"] = server_name
 
     if sentry_url:
-        sentry_sdk.init(sentry_url, release=VERSION, **kwargs)
+        sentry_sdk.init(sentry_url, release=VERSION, **sentry_kwargs)
 
     return logger
