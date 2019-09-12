db76052c7d62304fcd4f8ff86dc0b78e6d848836
DTailor <dann.croitoru@gmail.com>
2019-09-13 00:16:43 +0300

    damn...database

--- deneb/db.py
+++ deneb/db.py
@@ -132,7 +132,6 @@ class User(Model):  # type: ignore
     artists = fields.ManyToManyField("models.Artist", through="user_followed_artists")
     spotify_token = fields.CharField(max_length=255, null=True)
     created_at = fields.DatetimeField(auto_now_add=True)
-    updated_at = fields.DatetimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f"<spotify:user:{self.username}>"
