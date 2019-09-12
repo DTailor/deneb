b7bcb09de5ae196960b85ded8429545de62d87a3
DTailor <dann.croitoru@gmail.com>
2019-09-12 20:28:19 +0300

    fix-up migration to add updated at

--- alembic/versions/30f786bc9314_add_timestamp_to_user.py
+++ alembic/versions/30f786bc9314_add_timestamp_to_user.py
@@ -5,9 +5,10 @@ Revises: d00285e9d305
 Create Date: 2019-07-16 21:38:26.715520
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
+from sqlalchemy import func
+from sqlalchemy.sql import text
 
 # revision identifiers, used by Alembic.
 revision = "30f786bc9314"
@@ -17,12 +18,41 @@ depends_on = None
 
 
 def upgrade():
-    op.add_column("user", sa.Column("created_at", sa.DateTime, default=sa.func.now()))
     op.add_column(
-        "user", sa.Column("updated_at", sa.DateTime, onupdate=sa.func.sysdate())
+        "user", sa.Column("created_at", sa.DateTime, server_default=func.now())
+    )
+    op.add_column(
+        "user",
+        sa.Column("updated_at", sa.DateTime, server_default=func.now(), nullable=True),
+    )
+    conn = op.get_bind()
+    conn.execute(
+        text(
+            """
+            CREATE FUNCTION update_updated_at_column() RETURNS trigger
+                LANGUAGE plpgsql
+                AS $$
+            BEGIN
+                NEW.updated_at = NOW();
+                RETURN NEW;
+            END;
+            $$;
+
+            CREATE TRIGGER user_updated_at BEFORE UPDATE ON "user" FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
+            """
+        )
     )
 
 
 def downgrade():
     op.drop_column("user", "created_at")
     op.drop_column("user", "updated_at")
+    conn = op.get_bind()
+    conn.execute(
+        text(
+            """
+            DROP TRIGGER IF EXISTS user_updated_at ON "user";
+            DROP FUNCTION IF EXISTS update_updated_at_column;
+            """
+        )
+    )
