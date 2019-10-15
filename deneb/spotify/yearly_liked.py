2f98778a2620e9f7adaee973a953e36a288c4287
DTailor <dann.croitoru@gmail.com>
2019-10-15 21:13:44 +0300

    refactor for more explicit code yearly liked task

--- deneb/spotify/yearly_liked.py
+++ deneb/spotify/yearly_liked.py
@@ -1,6 +1,6 @@
 """Create spotify playlist with liked songs based on years"""
 import datetime
-from typing import List
+from typing import List, Optional
 
 from spotipy.client import SpotifyException
 
@@ -11,7 +11,7 @@ from deneb.logger import get_logger, push_sentry_error
 from deneb.sp import SpotifyYearlyStats, Spotter, spotify_client
 from deneb.spotify.common import (
     _get_to_update_users, _user_task_filter, fetch_all, fetch_user_playlists,
-    get_tracks, update_spotify_playlist
+    update_spotify_playlist
 )
 from deneb.structs import FBAlert, SpotifyKeys
 from deneb.tools import run_tasks, search_dict_by_key
@@ -32,34 +32,28 @@ def generate_tracks_to_add(
 
 
 async def _sync_with_spotify_playlist(
-    user: User, sp: Spotter, year: str, tracks: List[dict], dry_run: bool
+    user: User,
+    sp: Spotter,
+    new_tracks: List[dict],
+    playlist: dict,
+    playlist_name: str,
+    dry_run: bool,
 ) -> SpotifyYearlyStats:
-    playlist_name = generate_playlist_name(year)
-
-    user_playlists = await fetch_user_playlists(sp)
-    _LOGGER.info(f"updating playlist: <{playlist_name}> for {user}")
-
-    is_created, playlist = search_dict_by_key(playlist_name, user_playlists, "name")
-
-    playlist_tracks = await get_tracks(sp, playlist) if is_created else []
-
-    to_add_tracks = generate_tracks_to_add(tracks, playlist_tracks)
     if not dry_run:
-        has_new_tracks = bool(to_add_tracks)
-        if not is_created and has_new_tracks:
+        has_new_tracks = bool(new_tracks)
+        if not playlist and has_new_tracks:
             playlist = await sp.client.user_playlist_create(
                 sp.userdata["id"], playlist_name, public=False
             )
 
         if has_new_tracks:
             await update_spotify_playlist(
-                to_add_tracks, playlist["uri"], sp, insert_top=True
+                new_tracks, playlist["uri"], sp, insert_top=True
             )
 
-    if not playlist:
-        playlist = {"name": playlist_name}
+    _LOGGER.info(f"updated playlist: <{playlist['name']}> for {user}")
 
-    stats = SpotifyYearlyStats(user.fb_id, playlist, {"tracks": to_add_tracks})
+    stats = SpotifyYearlyStats(user.fb_id, playlist, {"tracks": new_tracks})
 
     _LOGGER.info(
         f"updated playlist: <{playlist_name}> for {user} | {stats.describe(brief=True)}"
@@ -67,8 +61,12 @@ async def _sync_with_spotify_playlist(
     return stats
 
 
-async def _fetch_saved_from_year_playlist(
-    user: User, sp: Spotter, year: str, dry_run: bool
+async def _fetch_year_tracks_from_saved(
+    user: User,
+    sp: Spotter,
+    year: str,
+    last_added_track_id: str = None,
+    dry_run: bool = False,
 ) -> List[dict]:
     current_year_tracks = []
     all_liked_partial = await sp.client.current_user_saved_tracks()
@@ -76,18 +74,60 @@ async def _fetch_saved_from_year_playlist(
     all_liked_songs = await fetch_all(sp, all_liked_partial)
 
     for item in all_liked_songs:
+        if item["track"]["id"] == last_added_track_id:
+            break
+
         if item["track"]["album"]["release_date"][:4] == year:
             current_year_tracks.append(item["track"])
+
     return current_year_tracks
 
 
 async def _handle_saved_songs_by_year_playlist(
     credentials: SpotifyKeys, user: User, year: str, dry_run: bool, fb_alert: FBAlert
 ) -> None:
+    """Main task runner which will:
+        - fetch playlist based on year and it's tracks
+        - fetch first first batch of tracks from playlist if exists
+        - first (which is last added) track is used as stop point for
+          fetching liked tracks
+        - fetch liked tracks up to last added track id previously fetched
+        - sync with playlist (and create if non-existent) new tracks
+    """
     try:
         async with spotify_client(credentials, user) as sp:
-            tracks = await _fetch_saved_from_year_playlist(user, sp, year, dry_run)
-            stats = await _sync_with_spotify_playlist(user, sp, year, tracks, dry_run)
+            # fetch playlist if exist and get last added track as reference
+            # to when to stop seeking in liked songs
+            playlist_name = generate_playlist_name(year)
+            user_playlists = await fetch_user_playlists(sp)
+
+            is_created, playlist = search_dict_by_key(
+                playlist_name, user_playlists, "name"
+            )
+
+            # not using `get_tracks` because there's need for the first item
+            # from first batch only
+            playlist_tracks = (
+                await sp.client.user_playlist(
+                    sp.userdata["id"], playlist["id"], fields="tracks,next"
+                )
+                if is_created
+                else []
+            )
+
+            # last added track if so
+            last_added_track_id = None
+            if playlist_tracks:
+                last_added_track_id = playlist_tracks[0]["track"]["id"]
+
+            _LOGGER.info(f"updating playlist: <{playlist_name}> for {user}")
+
+            new_tracks = await _fetch_year_tracks_from_saved(
+                user, sp, year, last_added_track_id, dry_run
+            )
+            stats = await _sync_with_spotify_playlist(
+                user, sp, new_tracks, playlist, playlist_name, dry_run
+            )
 
             if fb_alert.notify and stats.has_new_tracks():
                 await send_message(user.fb_id, fb_alert, stats.describe())
