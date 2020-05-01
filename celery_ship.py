021119e378d1dbf4b806a28d2803d84d63f2d2ed
DTailor <dann.croitoru@gmail.com>
2020-05-01 19:04:46 +0300

    docker compose init

---
+++ celery_ship.py
@@ -0,0 +1,77 @@
+import asyncio
+import os
+
+import uvloop
+from celery import Celery
+from celery.schedules import crontab
+from celery.signals import worker_process_init, worker_process_shutdown
+
+from deneb.config import VERSION
+from deneb.db import close_db, init_db
+from deneb.logger import get_logger
+from deneb.structs import FBAlert, SpotifyKeys
+from deneb.spotify.yearly_liked import update_users_playlists_liked_by_year
+
+_LOGGER = get_logger(__name__)
+
+SPOTIFY_KEYS = SpotifyKeys(
+    os.environ["SPOTIPY_CLIENT_ID"],
+    os.environ["SPOTIPY_CLIENT_SECRET"],
+    os.environ["SPOTIPY_REDIRECT_URI"],
+)
+
+# asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+uvloop.install()
+
+
+@worker_process_init.connect
+def init_worker(*args, **kwargs):
+    _LOGGER.info("Initializing +++++ database.")
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(init_db())
+
+
+@worker_process_shutdown.connect
+def shutdown_worker(**kwargs):
+    _LOGGER.info("Closing database !-------! for worker.")
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(close_db())
+
+
+# def make_celery():
+app = Celery(f"deneb-{VERSION}")
+
+
+@app.task()
+def liked_task():
+    _LOGGER.info("task run")
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        update_users_playlists_liked_by_year(
+            SPOTIFY_KEYS,
+            FBAlert(os.environ["FB_API_KEY"], os.environ["FB_API_URL"], True),
+            "dann.croitoru",
+            "2020",
+            "False",
+        )
+    )
+    print("print traskrun")
+
+
+config = {
+    "beat_schedule": {
+        "liked-sorted-yearly": {
+            "task": "celery_ship.liked_task",
+            "schedule": crontab(minute="*"),
+        },
+    },
+    "timezone": "Europe/Bucharest",
+}
+app.conf.update(config)
+# "args": (
+# SPOTIFY_KEYS,
+# FBAlert(os.environ["FB_API_KEY"], os.environ["FB_API_URL"], True),
+# "dann.croitoru",
+# "2020",
+# "False",
+# ),
